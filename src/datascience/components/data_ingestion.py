44e876aa707010b1f48a3a98bc027a2d4b3d2ba8
SohamBirlaa <sohambirla9210@gmail.com>
2026-04-07 04:22:38 +0530

    feat: add data ingestion pipeline with modular architecture

---
+++ src/datascience/components/data_ingestion.py
@@ -0,0 +1,32 @@
+import os
+import urllib.request as request
+from src.datascience import logger
+import zipfile
+
+from src.datascience.entity.config_entity import (DataIngestionConfig)
+## component-Data Ingestion
+class DataIngestion:
+    def __init__(self,config:DataIngestionConfig):
+        self.config=config
+    # downloading the zip file
+    def download_file(self):
+        if not os.path.exists(self.config.local_data_file):
+            filename, header = request.urlretrieve(
+                url = self.config.source_URL,
+                filename = self.config.local_data_file
+            )
+            logger.info(f"{filename} download! with the following info: /n{header}")
+        else:
+            logger.info(f"file is already exists")
+
+    def extract_zip_file(self):
+        """
+        zip_file_path: str
+        extracts the zip file into the data directory
+        function return None
+        """
+
+        unzip_path = self.config.unzip_dir
+        os.makedirs(unzip_path, exist_ok=True)
+        with zipfile.ZipFile(self.config.local_data_file, "r") as zip_ref:
+            zip_ref.extractall(unzip_path)
