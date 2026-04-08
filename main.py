e8c8348b53734033f85913489749003d5b31d347
SohamBirlaa <sohambirla9210@gmail.com>
2026-04-09 03:56:13 +0530

    feat: add data transformation stage to ML pipeline

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
-
+from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 STAGE_NAME = "Data Ingestion stage"
 
 try:
     logger.info(f">>> stage {STAGE_NAME} started <<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.initiate_data_ingestion()
-    logger.info(f">>>>> sage {STAGE_NAME} completed <<<<<\n\nx=============x")
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx=============x")
 except Exception as e:
     logger.exception(e)
     raise e
@@ -21,8 +21,20 @@ try:
     logger.info(f">>> stage {STAGE_NAME} started <<<")
     data_validation = DataValidationTrainingPipeline()
     data_validation.initiate_data_validation()
-    logger.info(f">>>>> sage {STAGE_NAME} completed <<<<<\n\nx=============x")
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx=============x")
 except Exception as e:
     logger.exception(e)
     raise e
 
+
+
+STAGE_NAME = "Data Transformation stage"
+
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.initiate_data_transformation()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<<\n\nx=========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/datascience/components/data_transformation.py
@@ -0,0 +1,25 @@
+import os
+from src.datascience import logger
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from src.datascience.entity.config_entity import DataTransformationConfig
+
+class DataTransformation:
+    def __init__(self, config: DataTransformationConfig):
+        self.config = config
+
+    def train_test_splitting(self):
+        data = pd.read_csv(self.config.data_path)
+
+        #splitting the data into traing and test sets (0.75, 0.25) splits
+        train, test = train_test_split(data, test_size=0.25, random_state=42)
+
+        train.to_csv(os.path.join(self.config.root_dir, "train.csv"), index=False)
+        test.to_csv(os.path.join(self.config.root_dir, "test.csv"), index=False)
+
+        logger.info("splitted data into traning and test sets")
+        logger.info(train.shape)
+        logger.info(test.shape)
+
+        print(train.shape)
+        print(test.shape)
